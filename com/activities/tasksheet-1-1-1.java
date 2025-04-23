89c938b3b9071ff63d6324accb73a946704f650d
CodeWithSubaru <86454768+CodeWithSubaru@users.noreply.github.com>
2025-04-23 11:20:27 +0000

    feat: add task sheet

---
+++ com/activities/tasksheet-1-1-1.java
@@ -0,0 +1,12 @@
+/**
+ * I've changed helloWorldApp -> HelloWorldApp // still compiles even small h
+ * rintLn -> println
+ * Kamusta Mundo!
+ */
+
+class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
