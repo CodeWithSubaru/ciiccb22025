1484dd265383199691c7a5217c7856bbc62e86b4
CodeWithSubaru <86454768+CodeWithSubaru@users.noreply.github.com>
2025-04-26 05:50:17 +0000

    feat: add Task111.java

---
+++ com/activities/Task111.java
@@ -0,0 +1,15 @@
+/**
+ * Compile with this cmd
+ * cd com/ 
+ * javac activities/Task111.java
+ * 
+ * Run with this cmd
+ * java activities/Task111
+ */
+package activities;
+
+class Task111 {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); 
+    }
+}
