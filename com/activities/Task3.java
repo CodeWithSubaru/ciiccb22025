571e9c732cd26ac73726983e250f2a0cc9fabf6e
CodeWithSubaru <86454768+CodeWithSubaru@users.noreply.github.com>
2025-05-02 07:16:53 +0000

    feat: add Task 3

---
+++ com/activities/Task3.java
@@ -0,0 +1,21 @@
+package com.activities;
+
+public class Task3 {
+    public static void main(String[] args) {
+        
+        String a = new String("Wow");
+        String b = "Wow";
+        b = a;
+        String c = a;
+        String d = c;
+        d = a + "!";
+        c = d;
+
+        boolean bl = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (bl && b2 && b3)
+            System.out.println("Success!");
+    }
+}
