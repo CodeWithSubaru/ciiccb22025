4616323b7fdeb4efbb8e2eff604eb7ca17b83fe0
CodeWithSubaru <86454768+CodeWithSubaru@users.noreply.github.com>
2025-05-02 07:07:34 +0000

    feat: add task 2

---
+++ com/activities/Task2.java
@@ -0,0 +1,23 @@
+package com.activities;
+
+public class Task2 {
+    
+    public static void main(String[] args) {
+        // char, byte, short, long, boolean, int
+        
+        char bigH = 'H';
+        byte thirtyOne = 31;
+        short ten = 10;
+        char w = 'w';
+        int zero = 0;
+        char r = 'r';
+        char l = 'l';
+        char d = 'd';
+        long twoPointZero = 2L;
+        boolean booleanTrue = true;
+        String output = bigH + "" + thirtyOne + "" + ten + " " + w + zero + r + l + d + " " + twoPointZero + '.' + zero + " " + booleanTrue;
+ 
+        System.out.println(output);
+    }
+
+}
