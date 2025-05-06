295fdca02e9075fd8557747d4bc15485d5f201b6
CodeWithSubaru <86454768+CodeWithSubaru@users.noreply.github.com>
2025-05-06 10:15:23 +0000

    feat: add Multiplication Table

---
+++ com/new_programming_challenge/multiplication_table/Main.java
@@ -0,0 +1,15 @@
+package com.new_programming_challenge.multiplication_table;
+
+public class Main {
+    public static void main(String[] args) {
+        final byte MAX_NUM = 10;
+
+        for(int row = 1; row <= MAX_NUM; row++) {
+            for(int col = 1; col <= MAX_NUM; col++)
+                System.out.print((row*col) + "\t");
+                
+            System.out.println();
+        }
+
+    }
+}
