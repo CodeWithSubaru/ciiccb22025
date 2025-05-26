956240c08667e67e096f0186986ea139f356be2f
CodeWithSubaru <francis.cloydm@gmail.com>
2025-05-26 12:15:23 +0800

    fix: fixing cumulative sum

--- com/activities/Task8.java
+++ com/activities/Task8.java
@@ -1,15 +1,21 @@
 package activities;
 
 public class Task8 {
-	static int sum;
+	
 	public static void main(String[] args) {
 		int sumOfAll = sum(4,5,10);
-		System.out.println(sumOfAll);
+		System.out.println("Total sum of all cumulative numbers" + " = " + sumOfAll);
 	}
 	
 	public static int sum(int... numbers) {
-		for(int number: numbers)
-			sum += number;
-		return sum;
+		int total = 0;
+		
+		for (int number : numbers) {
+			int sumOfNumber = (number * (number + 1)) / 2;
+			System.out.println("Cumulative sum for " + number + " = " + sumOfNumber);
+			total += sumOfNumber;
+		}
+		
+		return total;
 	}
 }
