ad8c4e48162d13bcf9edf619c3e2f355f5ef3d93
CodeWithSubaru <francis.cloydm@gmail.com>
2025-06-05 00:16:39 +0800

    feat: add new validation

--- com/jobsheet/job_sheet_2_4/CashTransfer.java
+++ com/jobsheet/job_sheet_2_4/CashTransfer.java
@@ -13,13 +13,20 @@ public class CashTransfer {
 	
 	public static void cashTransfer(BigDecimal amount, String name, int accountId, Date date, int transferToId, int transferFromId) {
 		try {
+			if (amount.compareTo(BigDecimal.ZERO) <= 0)
+				throw new Exception("Error: Amount must be greater than zero.");
+			
+			if (transferFromId == transferToId)
+				throw new Exception("Error: Cannot transfer to the same account.");
+			
 			db = DB.getConnection();
 			try (ResultSet rsFrom = selectBalance(transferFromId)) {
 				try (ResultSet rsTo = selectBalance(transferToId)) {
 					
 					if (rsFrom != null && rsTo != null && rsFrom.next() && rsTo.next()) {
-						if (rsFrom.getBigDecimal("amount").compareTo(BigDecimal.ZERO) <= 0)
-							throw new Exception("Error: Transaction failed. Your balance is zero.");
+						
+						if (rsFrom.getBigDecimal("amount").compareTo(amount) < 0)
+							throw new Exception("Error: Insufficient balance.");
 						
 						query = "INSERT INTO TRANSACTIONS (amount, name, account_id, date, transferToId, transferFromId) VALUES (?, ?, ?, ?, ?, ?)";
 						stmt = db.prepareStatement(query);
